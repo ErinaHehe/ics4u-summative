353a29c4cc09c9d89ccfafe58a298a5f3e6ad28e
Erina He <erina.he@student.tdsb.on.ca>
2024-12-10 11:30:47 -0500

    firebase added

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,6 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCt-D7Is7MrHoDrnz4fOvbE2sZXfJH4dAU",
+    authDomain: "summative-2f427.firebaseapp.com",
+    projectId: "summative-2f427",
+    storageBucket: "summative-2f427.firebasestorage.app",
+    messagingSenderId: "298840091428",
+    appId: "1:298840091428:web:a06654f79fa48f03eba1b1"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
