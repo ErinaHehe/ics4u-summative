bae2ec4344322bf5cb933680b56a9bd011ecd40d
Erina He <erina.he@student.tdsb.on.ca>
2025-01-07 10:41:18 -0500

    added protected routes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StoreProvider } from "./context/index.jsx";
 import HomeView from "../src/views/HomeView.jsx";
 import DetailView from "../src/views/DetailView.jsx";
 import GenreView from "../src/views/GenreView.jsx";
@@ -7,7 +8,7 @@ import MoviesView from "../src/views/MoviesView.jsx";
 import RegisterView from "../src/views/RegisterView.jsx";
 import CartView from "../src/views/CartView.jsx";
 import SettingView from "../src/views/SettingView.jsx";
-import { StoreProvider } from "./context/index.jsx";
+import ProtectedRoutes from "./util/ProtectedRoutes";
 import './App.css';
 
 function App() {
@@ -19,11 +20,13 @@ function App() {
           <Route path="/" element={<HomeView />} />
           <Route path="/register" element={<RegisterView />} />
           <Route path="/login" element={<LoginView />} />
-          <Route path="/cart" element={<CartView />} />
-          <Route path="/setting" element={<SettingView />} />
-          <Route path="/movies" element={<MoviesView />}>
-            <Route path="/movies/genre/:genre_id" element={<GenreView />} />
-            <Route path="/movies/details/:id" element={<DetailView />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/cart" element={<CartView />} />
+            <Route path="/setting" element={<SettingView />} />
+            <Route path="/movies" element={<MoviesView />}>
+              <Route path="/movies/genre/:genre_id" element={<GenreView />} />
+              <Route path="/movies/details/:id" element={<DetailView />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
