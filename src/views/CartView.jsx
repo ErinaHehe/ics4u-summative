c9cb42fa249b6dce6c8f5b3e69f9f5baa6cc6853
Erina He <erina.he@student.tdsb.on.ca>
2025-01-07 11:00:09 -0500

    copied Mr. Qayum's code

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -1,12 +1,25 @@
 import { useStoreContext } from "../context";
+import { firestore } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
 import "./Cartview.css";
 
 function CartView() {
   const { cart, setCart } = useStoreContext();
 
+  const checkout = async () => {
+    const docRef = doc(firestore, "users", user.uid);
+    await setDoc(docRef, cart.toJS());
+  }
+
+    // Code to read from Friestore
+    // const docRef = doc(firestore, "users", user.uid);
+    // const data = (await getDoc(docRef)).data();
+    // const cart = Map(data);
+
   return (
     <div className="cart-view">
       <h1>Shopping Cart</h1>
+      <button onClick={() => checkout()}>Checkout</button>
       <div className="cart-items">
         {
           cart.entrySeq().map(([key, value]) => {
